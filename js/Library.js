887c767d3c6286e8e5bf312ffde81ae8e44347ec
Arian <arianbtco@gmail.com>
2024-11-21 14:24:18 +0330

    change url images

--- js/Library.js
+++ js/Library.js
@@ -77,23 +77,23 @@ let img_change2 = $.querySelector('.img_change2')
 let imgWatch = $.querySelector('.img_watch')
 
 img_change.addEventListener('mouseover', function () {
-    img_change.setAttribute('src', '../Image/73409683bedd334608bd3aff7c048fcddc3094ed_1653804755-300x300.jpg')
+    img_change.setAttribute('src', './Image/73409683bedd334608bd3aff7c048fcddc3094ed_1653804755-300x300.jpg')
 })
 img_change.addEventListener('mouseleave', function () {
-    img_change.setAttribute('src', '../Image/موبایل-اپل-آیفون-13-ظرفیت-128-گیگابایت-4-460x460.png')
+    img_change.setAttribute('src', './Image/موبایل-اپل-آیفون-13-ظرفیت-128-گیگابایت-4-460x460.png')
 })
 
 img_change2.addEventListener('mouseover', function () {
-    img_change2.setAttribute('src', '../Image/73409683bedd334608bd3aff7c048fcddc3094ed_1653804755-300x300.jpg')
+    img_change2.setAttribute('src', './Image/73409683bedd334608bd3aff7c048fcddc3094ed_1653804755-300x300.jpg')
 })
 img_change2.addEventListener('mouseleave', function () {
-    img_change2.setAttribute('src', '../Image/iphone-12-white-select-2020-866x1024-1-460x460.png')
+    img_change2.setAttribute('src', './Image/iphone-12-white-select-2020-866x1024-1-460x460.png')
 })
 imgWatch.addEventListener('mouseover', function () {
-    imgWatch.setAttribute('src', '../Image/2-10-600x600-1-300x300.jpg')
+    imgWatch.setAttribute('src', './Image/2-10-600x600-1-300x300.jpg')
 })
 imgWatch.addEventListener('mouseleave', function () {
-    imgWatch.setAttribute('src', '../Image/wach-apple-460x460.png')
+    imgWatch.setAttribute('src', './Image/wach-apple-460x460.png')
 })
 
 
